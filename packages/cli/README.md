5b90d7f66f6c85861f0a7f94e0a505fa46f771ae
Tim Smart <tim.smart@arisechurch.com>
2020-05-24 19:44:10 +1200

    Add cli

---
+++ packages/cli/README.md
@@ -0,0 +1,15 @@
+@fpk/cli
+========
+
+CLI for fpk - functional configuration tool
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/@fpk/cli.svg)](https://npmjs.org/package/@fpk/cli)
+[![Downloads/week](https://img.shields.io/npm/dw/@fpk/cli.svg)](https://npmjs.org/package/@fpk/cli)
+[![License](https://img.shields.io/npm/l/@fpk/cli.svg)](https://github.com/tim-smart/fpk/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+<!-- commands -->
---
+++ packages/cli/src/index.ts
@@ -0,0 +1,52 @@
+import { availableFormats, generate } from "@fpk/core";
+import { Command, flags } from "@oclif/command";
+import * as fs from "fs/promises";
+import { safeLoad } from "js-yaml";
+
+class FpkCli extends Command {
+  static description = "Generate configuration from an fpk config tree";
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    source: flags.string({
+      char: "d",
+      description: "source directory",
+      required: true,
+    }),
+    output: flags.string({
+      char: "o",
+      description: "output directory",
+      required: true,
+    }),
+    context: flags.string({
+      char: "c",
+      description: "yaml file to load context configuration from",
+    }),
+    format: flags.enum({
+      char: "f",
+      options: availableFormats(),
+      default: "yaml",
+    }),
+  };
+
+  static args = [];
+
+  async run() {
+    const { flags } = this.parse(FpkCli);
+    let context = {};
+
+    if (flags.context) {
+      const contextBlob = await fs.readFile(flags.context);
+      context = safeLoad(contextBlob.toString("utf8"));
+    }
+
+    await generate(flags.source, flags.output, {
+      context,
+      format: flags.format,
+    });
+  }
+}
+
+export = FpkCli;
