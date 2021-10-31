d2c2ec2ef4316ebc4c0670675d0015ebf0edc01f
Tim Smart <tim.smart@arisechurch.com>
2021-10-31 14:55:25 +1300

    Add more hpa functions

--- packages/k8s/src/hpa.ts
+++ packages/k8s/src/hpa.ts
@@ -1,7 +1,11 @@
+import { flow } from "fp-ts/lib/function";
 import {
-  HorizontalPodAutoscaler,
   CrossVersionObjectReference,
+  HorizontalPodAutoscaler,
+  MetricSpec,
+  MetricTarget,
 } from "kubernetes-types/autoscaling/v2beta2";
+import * as R from "ramda";
 import { DeepPartial } from "./common";
 import { maybeMergeResource, resource } from "./resources";
 
@@ -19,3 +23,58 @@ export const hpa = (
     ),
     toMerge,
   );
+
+interface IReplicaRangeCompatable {
+  spec?: {
+    minReplicas?: number;
+    maxReplicas?: number;
+  };
+}
+
+export const setReplicaRange =
+  (min: number, max: number) =>
+  <T extends IReplicaRangeCompatable>(input: T) =>
+    R.over(
+      R.lensProp("spec"),
+      flow(R.assoc("minReplicas", min), R.assoc("maxReplicas", max)),
+      input,
+    );
+
+interface IAddAutoscalerMetricsCompatable {
+  spec: {
+    metrics: MetricSpec[];
+  };
+}
+
+export const addAutoscalerMetric =
+  (metric: MetricSpec) =>
+  <T extends DeepPartial<IAddAutoscalerMetricsCompatable>>(resource: T): T =>
+    R.over(
+      R.lensProp("spec"),
+      flow(
+        R.defaultTo<any>({ metrics: [] }),
+        R.over(
+          R.lensProp<
+            IAddAutoscalerMetricsCompatable["spec"],
+            keyof IAddAutoscalerMetricsCompatable["spec"]
+          >("metrics"),
+          flow(R.defaultTo([]), R.append(metric)),
+        ),
+      ),
+      resource,
+    );
+
+export const addAutoscalerCPUTarget = (target: MetricTarget) =>
+  addAutoscalerMetric({
+    type: "Resource",
+    resource: {
+      name: "cpu",
+      target,
+    },
+  });
+
+export const addTargetCPUUtilization = (averageUtilization: number) =>
+  addAutoscalerCPUTarget({
+    type: "Utilization",
+    averageUtilization,
+  });
--- packages/k8s/test/hpa.test.ts
+++ packages/k8s/test/hpa.test.ts
@@ -1,6 +1,7 @@
 import * as K from "../src/index";
 import { describe } from "mocha";
 import { runCases } from "./helpers";
+import { HorizontalPodAutoscaler } from "kubernetes-types/autoscaling/v2beta2";
 
 describe("hpa", () =>
   runCases([
@@ -42,5 +43,76 @@ describe("hpa", () =>
           },
         },
       },
+    },
+    {
+      it: "addAutoscalerMetric adds a metric",
+      in: K.hpa("myhpa", {
+        apiVersion: "v1",
+        kind: "Deployment",
+        name: "myapp",
+      }),
+      fn: K.addAutoscalerMetric({ type: "Resource" }),
+      diff: {
+        spec: {
+          metrics: [
+            {
+              type: "Resource",
+            },
+          ],
+        },
+      } as HorizontalPodAutoscaler,
+    },
+    {
+      it: "addAutoscalerCPUTarget adds a metric",
+      in: K.hpa("myhpa", {
+        apiVersion: "v1",
+        kind: "Deployment",
+        name: "myapp",
+      }),
+      fn: K.addAutoscalerCPUTarget({
+        type: "Utilization",
+        averageUtilization: 75,
+      }),
+      diff: {
+        spec: {
+          metrics: [
+            {
+              type: "Resource",
+              resource: {
+                name: "cpu",
+                target: {
+                  type: "Utilization",
+                  averageUtilization: 75,
+                },
+              },
+            },
+          ],
+        },
+      } as HorizontalPodAutoscaler,
+    },
+    {
+      it: "addTargetCPUUtilization adds a metric",
+      in: K.hpa("myhpa", {
+        apiVersion: "v1",
+        kind: "Deployment",
+        name: "myapp",
+      }),
+      fn: K.addTargetCPUUtilization(5),
+      diff: {
+        spec: {
+          metrics: [
+            {
+              type: "Resource",
+              resource: {
+                name: "cpu",
+                target: {
+                  type: "Utilization",
+                  averageUtilization: 5,
+                },
+              },
+            },
+          ],
+        },
+      } as HorizontalPodAutoscaler,
     },
   ]));
